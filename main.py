636c07922d00c94b6a3a3ee131f0348cee21e93f
vke007 <161368166+vke007@users.noreply.github.com>
2026-02-19 08:28:05 +0530

    Change default port from 5000 to 8080

--- main.py
+++ main.py
@@ -820,6 +820,6 @@ def init_db():
 
 if __name__ == "__main__":
     init_db()
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     print(f"🚀  Listening on :{port}")
     app.run(host="0.0.0.0", port=port, debug=False)
