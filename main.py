db9f3c5fe5845a88986b172795919aa36688b984
vke007 <161368166+vke007@users.noreply.github.com>
2026-02-19 11:44:24 +0530

    Add ping endpoint to check server status

--- main.py
+++ main.py
@@ -822,4 +822,7 @@ if __name__ == "__main__":
         db.create_all()  # This ensures your tables exist
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+  @app.get("/api/ping")
+def ping():
+    return jsonify({"status": "online"})
   
